d3e9b0094a5948f68541722ef6ea74bfc23ab3ab
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-04-08 19:04:36 +0200

    WIP landing page

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -6,9 +6,10 @@ import React, { useContext } from 'react'
 export const Navbar = () => {
     
   return (
-    <div className="navbar bg-base-100 px-5">
+    <div className="navbar glass fixed  px-5">
         <div className="navbar-start">
             <div className="dropdown">
+            
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
@@ -25,7 +26,8 @@ export const Navbar = () => {
                 <li><Link href="/faq">FAQ</Link></li>
             </ul>
             </div>
-            <a className="btn btn-ghost text-xl">DiMenager</a>
+            <Link className="btn btn-ghost text-xl" href="/">DiMenager</Link>
+            
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <button className="btn btn-primary">Button</button>
+    <main className="">
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://www.purepc.pl/image/news/2023/03/09_discord_testuje_mozliwosci_sztucznej_inteligencji_ai_odpowie_na_pytania_uzytkownikow_i_pomoze_w_moderacji_serwera_1_b.png)'}}>
+        <div className="hero-overlay bg-opacity-70"></div>
+        <div className="hero-content text-center">
+          <div className="max-w-2xl">
+            <h1 className="text-6xl font-bold py-5">Your All-in-One Project Management Solution Integrated with Discord!</h1>
+            <button className="btn btn-wide btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero min-h-[80vh] bg-base-150">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
